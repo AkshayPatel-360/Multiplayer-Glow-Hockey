3deab81d9691b4cd6ae114a298268afc3125be8f
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-10-27 20:23:06 -0400

    Update PuckScript.cs

--- Assets/Resources/Scripts/PuckScript.cs
+++ Assets/Resources/Scripts/PuckScript.cs
@@ -35,7 +35,7 @@ public class PuckScript : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "PlayerGoal")
         {
